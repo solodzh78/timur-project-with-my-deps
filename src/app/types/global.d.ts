2cdd1affdbb27beba6e348572879a2245cad0989
Sergei Solodzhuk <solodzh@gmail.com>
2023-02-19 21:13:05 +0200

    replace global.d.ts

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,19 +1,18 @@
 declare module '*.scss' {
-    interface IClassNames {
-        [className: string]: string
-    }
-    const classNames: IClassNames;
-    export = classNames;
+  const classNames: Record<string, string>;
+  export = classNames;
+}
+
+declare module '*.svg' {
+  import { SVGProps, VFC } from 'react';
+
+  const content: VFC<SVGProps<SVGSVGElement>>;
+  export default content;
 }
 
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
-declare module '*.svg' {
-    import React from 'react';
 
-    const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
-    export default SVG;
-}
-
+// eslint-disable-next-line no-unused-vars
 declare const __IS_DEV__: boolean;

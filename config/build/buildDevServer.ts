480d8b75f9a9dc6d9cc50f8d91025e3de306071b
Sergei Solodzhuk <solodzh@gmail.com>
2023-02-19 21:01:28 +0200

    replace buildDevServer buildPlugins buildResolvers

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -1,11 +1,14 @@
-import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
+import type {
+  Configuration as DevServerConfiguration,
+} from 'webpack-dev-server';
+
 import { BuildOptions } from './types/config';
 
-export function buildDevServer(options: BuildOptions): DevServerConfiguration {
-    return {
-        port: options.port,
-        open: true,
-        historyApiFallback: true,
-        hot: true,
-    };
+export function buildDevServer({ port }: BuildOptions): DevServerConfiguration {
+  return {
+    port,
+    open: true,
+    historyApiFallback: true,
+    hot: true,
+  };
 }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,30 +1,39 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import {
+  DefinePlugin,
+  HotModuleReplacementPlugin,
+  ProgressPlugin,
+  WebpackPluginInstance,
+} from 'webpack';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
-export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-    const plugins = [
-        new HtmlWebpackPlugin({
-            template: paths.html,
-        }),
-        new webpack.ProgressPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css',
-        }),
-        new webpack.DefinePlugin({
-            __IS_DEV__: JSON.stringify(isDev),
-        }),
-    ];
+// eslint-disable-next-line max-len
+export function buildPlugins({ paths, isDev, bundleAnalyze }: BuildOptions): WebpackPluginInstance[] {
+  const plugins: WebpackPluginInstance[] = [
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
+    new ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    }),
+    new DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+  ];
 
-    if (isDev) {
-        plugins.push(new webpack.HotModuleReplacementPlugin());
-        plugins.push(new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }));
-    }
+  if (isDev) {
+    plugins.push(new HotModuleReplacementPlugin());
+    plugins.push(new ReactRefreshWebpackPlugin({ overlay: false }));
+  }
 
-    return plugins;
+  if (bundleAnalyze) {
+    plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: true }));
+  }
+
+  return plugins;
 }

2cc7fbaaf73d4d93b045bf71e04edc9ed7396d87
Sergei Solodzhuk <solodzh@gmail.com>
2023-02-19 18:58:40 +0200

    delete bundleAnalizer from webpack.config

--- webpack.config.ts
+++ webpack.config.ts
@@ -14,14 +14,14 @@ export default (env: BuildEnv) => {
   const mode = env.mode || 'development';
   const isDev = mode === 'development';
   const PORT = env.port || 3000;
-  const bundleAnalyze = env.bundleAnalyze || false;
+//   const bundleAnalyze = env.bundleAnalyze || false;
 
   const config: webpack.Configuration = buildWebpackConfig({
     mode,
     isDev,
     paths,
     port: PORT,
-    bundleAnalyze,
+    // bundleAnalyze,
   });
 
   return config;

34c72aa3ede197fea9c7fc36ce2c8183b808b295
Sergei Solodzhuk <solodzh@gmail.com>
2023-02-19 19:05:49 +0200

    replace buildWebpackConfig

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,28 +1,31 @@
-import webpack from 'webpack';
-import path from 'path';
-import { BuildOptions } from './types/config';
-import { buildPlugins } from './buildPlugins';
-import { buildLoaders } from './buildLoaders';
-import { buildResolvers } from './buildResolvers';
+import { Configuration } from 'webpack';
 import { buildDevServer } from './buildDevServer';
+import { buildLoaders } from './buildLoaders';
+import { buildPlugins } from './buildPlugins';
+import { buildResolvers } from './buildResolvers';
+import { BuildOptions } from './types/config';
 
-export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const { paths, mode, isDev } = options;
+export function buildWebpackConfig(options: BuildOptions): Configuration {
+  const { paths, mode, isDev } = options;
+  const config: Configuration = {
+    mode,
+    entry: paths.entry,
+    output: {
+      filename: '[name].[contenthash].js',
+      path: paths.build,
+      clean: true,
+    },
+    module: {
+      rules: buildLoaders(options),
+    },
+    plugins: buildPlugins(options),
+    resolve: buildResolvers(options),
+  };
 
-    return {
-        mode,
-        entry: paths.entry,
-        output: {
-            filename: '[name].[contenthash].js',
-            path: paths.build,
-            clean: true,
-        },
-        plugins: buildPlugins(options),
-        module: {
-            rules: buildLoaders(options),
-        },
-        resolve: buildResolvers(options),
-        devtool: isDev ? 'inline-source-map' : undefined,
-        devServer: isDev ? buildDevServer(options) : undefined,
-    };
+  if (isDev) {
+    config.devServer = buildDevServer(options);
+    config.devtool = 'inline-source-map';
+  }
+
+  return config;
 }
